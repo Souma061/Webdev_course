5fc6bfc67c6fb16e153e2da824fea659350036a9
Souma061 <soumabrataghosh57@gmail.com>
2025-05-23 22:53:17 +0530

    add problems and solutions for Sigma Developers;

---
+++ javascript mastery/basics/operations.js
@@ -0,0 +1,24 @@
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+let name = "John";
+let age = 30;
+console.log(`my name is ${name} and I am ${age} years old`);
+
+let employee = {
+  name: "John",
+  age: 30,
+  position: "Developer",
+}
+
+
+
